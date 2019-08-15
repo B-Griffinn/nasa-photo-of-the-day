c730c56b0bc75ec55443b185bac72fd473b74419
Ben <bigriffin94@outlook.com>
2019-08-14 19:07:35 -0700

    M

--- src/components/ExplanationComponents/ExlpanationCard.jsx
+++ src/components/ExplanationComponents/ExlpanationCard.jsx
@@ -4,9 +4,9 @@ import React from "react";
 const ExplainProps = props => {
      // NOTE: The value given to setState() must be of the same type as your value that is expected to be
      return (
-        <div className="explanation">
+        <React.Fragment>
             <p>{props.explanation}</p>
-        </div>
+        </React.Fragment>
      )
 }
 
